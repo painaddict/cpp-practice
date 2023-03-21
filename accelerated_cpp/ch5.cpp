5143d5ff10d8fcda0cd79e88620ef08e543314c6
painaddict <115467129+painaddict@users.noreply.github.com>
2023-03-21 10:32:42 +0530

    added q9

--- accelerated_cpp/ch5.cpp
+++ accelerated_cpp/ch5.cpp
@@ -17,25 +17,25 @@ int check_palindrome(string input) {
 }
 
 // q6
-vector<Student_info> extract_fails(vector<Student_info>& students) {
-	vector<Student_info> fail;
-	vector<Student_info>::size_type i = 0;
-	vector<Student_info>::size_type count = 0;
+// vector<Student_info> extract_fails(vector<Student_info>& students) {
+// 	vector<Student_info> fail;
+// 	vector<Student_info>::size_type i = 0;
+// 	vector<Student_info>::size_type count = 0;
 
-	// invariant: elements [0, i) of students represent passing grades
-	while (i != students.size()) {
-		if (fgrade(students[i])) {
-			// fail.push_back(students[i]);
-			// students.erase(students.begin() + i);
-			students.insert(students.begin(), students[i]);
-			count++;
-		} else
-			++i;
-	}
-	// resize after the process
-	students.resize(count);	
-return fail;
-}
+// 	// invariant: elements [0, i) of students represent passing grades
+// 	while (i != students.size()) {
+// 		if (fgrade(students[i])) {
+// 			// fail.push_back(students[i]);
+// 			// students.erase(students.begin() + i);
+// 			students.insert(students.begin(), students[i]);
+// 			count++;
+// 		} else
+// 			++i;
+// 	}
+// 	// resize after the process
+// 	students.resize(count);	
+// return fail;
+// }
 
 //q9 
 int check_case(string word) {
@@ -55,13 +55,27 @@ int q9() {
 	// followed by the uppercase words.
 	vector<string> inputs;
 	string x;
+	cout << "Enter Strings until EOF:-" << endl;
 	while (cin >> x) {
 		inputs.push_back(x);
 	}
-
+	for (int i = 0; i < inputs.size(); i++) {
+		x = inputs[i];
+		if (check_case(x) == 1) {
+			inputs.push_back(x);
+			inputs.erase(inputs.begin() + i);
+		}
+	}
+	// display the result
+	vector<string>::const_iterator iter;
+	for (iter = inputs.begin(); iter <= inputs.end(); iter++) {
+		cout << *iter << endl;
+	}
+	return 0;
 }
 
 int main() {
 	check_palindrome("plisilp");
+	q9();
 	return 0;
 }
