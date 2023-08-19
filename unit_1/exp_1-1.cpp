dffe500effe88965cabc8e3918abe6a350491e7c
serv0id <113516593+serv0id@users.noreply.github.com>
2023-08-19 20:31:31 +0530

    question 1.1

--- unit_1/exp_1-1.cpp
+++ unit_1/exp_1-1.cpp
@@ -2,7 +2,7 @@
 
 // WAP to insert values in an array (sorted list) [DO NOT USE VECTOR]
 
-int q_1(void) {
+void q_1(void) {
 	std::cout << "Input the size of the array as an int: ";
 	
 	// take the input size and initialise an array
@@ -13,10 +13,37 @@ int q_1(void) {
 						 // to be inserted in the future.
 
 	// take the array elements as input
+	int buffer;
 	std::cout << "Enter the elements separated by a space: ";
 	for (int i = 0; i < size; i++) {
-
+		std::cin >> buffer;
+		array[i] = buffer;
 	}
 
+	// replace last element to be 0 (to account for garbage)
+	array[size + 1] = 0;
 
+	std::cout << "Enter the element to be inserted and at which index: ";
+	int pos, value;
+
+	std::cin >> value >> pos;
+	
+	// move all elements after (pos)th index to their next index
+	for (int i = size + 1; i > pos; i--) {
+		array[i] = array[i - 1];
+	}
+
+	// insert element
+	array[pos] = value;
+
+	// print updated element
+	for (int i = 0; i < size + 1; i++) {
+		std::cout << array[i] << std::endl;
+	}
+}
+
+int main(void) {
+	q_1();
+
+	return 0;
 }
