fed59f0b0fa6faf69b128b30fd29eb4e28d985cb
painaddict <115467129+painaddict@users.noreply.github.com>
2023-08-26 22:18:05 +0530

    incomplete....

---
+++ misc/insertion_sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void) {
+	// Demonstrate an implementation of the insertion 
+	// sort algorithm.
+
+	int array[5] = {2, 1, 99, 471, 0};
+	int size = sizeof(array) / sizeof(array[0]);
+
+	int key = array[1];
+
+	for (int i = 0; i < size; i++) {
+		if (array[0] > )
+	}
+
+	return 0;
+}
