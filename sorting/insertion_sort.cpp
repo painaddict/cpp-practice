f00f1964099f330a64d79b38e3e26192efb52a03
serv0id <113516593+serv0id@users.noreply.github.com>
2023-09-21 06:20:01 +0530

    implement insertion sort

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -7,11 +7,24 @@ int main(void) {
 	int array[5] = {2, 1, 99, 471, 0};
 	int size = sizeof(array) / sizeof(array[0]);
 
-	int key = array[1];
+	// O(n^2) 
+	// Iterate through each element and compare it with "left". 
+	// Swap if needed
 
-	for (int i = 0; i < size; i++) {
-		if (array[0] > )
+	int temp;
+	
+	for (int i = 1; i < size; i++) {
+		for (int j = i; j > 0; j--) {
+			if (array[j] < array[j - 1]) {
+				// swap routine
+				temp = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = temp;
+			}
+		}
 	}
-
-	return 0;
+	// print the result
+	for (int i = 0; i < size; i++) {
+		std::cout << array[i] << " " << std::endl;
+	}
 }
