75ae65d0826270fad85c2167a4680e633b5471d5
serv0id <113516593+serv0id@users.noreply.github.com>
2023-12-04 04:18:26 +0530

    added doubly linked list

--- linked-list/base.cpp
+++ linked-list/base.cpp
@@ -9,5 +9,26 @@ int main(void) {
 	head->next_node = element_1;
 	head->store = 69;
 
-	std::cout << element_1->next_node; // 0 (null)
+	std::cout << element_1->next_node << std::endl; // 0 (null)
+
+    // create node_doubles
+    node_double * double_head = new node_double();
+    node_double * one = new node_double();
+    node_double * two = new node_double();
+
+    double_head->left = NULL;
+    double_head->right = one;
+
+    one->left = double_head;
+    one->right = two;
+
+    two->left = one;
+    two->right = NULL;
+
+    // set data
+
+    one->store = 69;
+    two->store = 70;
+
+    std::cout << double_head->right->right->store;
 }
--- linked-list/defs.h
+++ linked-list/defs.h
@@ -8,3 +8,16 @@ typedef struct Node {
 		next_node = NULL; // define default
 	}
 } node;
+
+// doubly linked list
+
+typedef struct Node2 {
+    int store;
+    struct Node2 * left;
+    struct Node2 * right;
+
+    Node2() {
+        left = NULL;
+        right = NULL;
+    }
+} node_double;
